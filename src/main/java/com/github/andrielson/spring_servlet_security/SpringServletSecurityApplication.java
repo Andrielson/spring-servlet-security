496430bf8ff1636b46fb20640f58d29c885fc2b1
Andrielson Silva <andrielson@gmail.com>
2022-09-17 15:48:47 -0300

    Initial commit

---
+++ src/main/java/com/github/andrielson/spring_servlet_security/SpringServletSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.spring_servlet_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServletSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServletSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/andrielson/spring_servlet_security/SpringServletSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.spring_servlet_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServletSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
